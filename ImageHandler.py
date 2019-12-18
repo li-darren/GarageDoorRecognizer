835dffbce25112ff737eb95e9ddda9a79decc87b
li-darren <d338li@edu.uwaterloo.ca>
2019-12-18 00:38:30 -0500

    added windows to see imagehandler process files

--- ImageHandler.py
+++ ImageHandler.py
@@ -22,7 +22,7 @@ def check_garage_doors_open (root_folder):
     
     light_red1 = np.array([0, 10, 20], dtype="uint8")
     dark_red1 = np.array([20, 255, 255], dtype="uint8")
-    light_red2 = np.array([170, 25, 20], dtype="uint8")
+    light_red2 = np.array([160, 10, 20], dtype="uint8")
     dark_red2 = np.array([180, 255, 255], dtype="uint8")
 
 
@@ -62,9 +62,13 @@ def check_garage_doors_open (root_folder):
 
 
     cv2.namedWindow("imagewindow", cv2.WINDOW_NORMAL)
-    cv2.imshow('imagewindow', hsv_img)
-    cv2.namedWindow("croppedimg", cv2.WINDOW_NORMAL)
-    cv2.imshow('croppedimg', cropped_img_left)
+    cv2.imshow('imagewindow', img)
+    cv2.namedWindow("hsvimagewindow", cv2.WINDOW_NORMAL)
+    cv2.imshow('hsvimagewindow', hsv_img)
+    cv2.namedWindow("croppedimgleft", cv2.WINDOW_NORMAL)
+    cv2.imshow('croppedimgleft', cropped_img_left)
+    cv2.namedWindow("croppedimgright", cv2.WINDOW_NORMAL)
+    cv2.imshow('croppedimgright', cropped_img_right)
     cv2.namedWindow("maskedwindow", cv2.WINDOW_NORMAL)
     cv2.imshow('maskedwindow', mask)
     cv2.waitKey(0)
