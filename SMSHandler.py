31b3366630c129fb1b88f2a1c41824ebcde834e4
Darren Li <darren.li@genesys.com>
2019-12-18 11:51:00 -0500

    changed phone numbers

--- SMSHandler.py
+++ SMSHandler.py
@@ -5,8 +5,8 @@ def send_sms (door_open, account_sid, auth_token):
        client = Client(account_sid, auth_token)
 
        message = client.messages.create(
-              from_='whatsapp:+14155238886',
+              from_='+12019924112',
               body="Oh no! %s doors is/are open." % door_open,
-              to='whatsapp:+16477170038',
+              to='+16477170038',
        )
        print("Sent SMS Alert!")
