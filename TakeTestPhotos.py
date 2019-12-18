dc8b3a9479c96c8711e7b257cec4695bba9c667c
li-darren <d338li@edu.uwaterloo.ca>
2019-12-17 21:52:22 -0500

    changed test take photos script to work with iso and automatically create folder

--- TakeTestPhotos.py
+++ TakeTestPhotos.py
@@ -7,14 +7,14 @@ import os
 
 root_folder = os.path.dirname(os.path.abspath(__file__)).replace("\\","/")
 
-num_folders = os.listdir(root_folder + "/TestImages")
-number_files = len(num_folders)
-
+folder = os.listdir(root_folder + "/TestImages")
+num_folder_items = len(folder)
+os.mkdir(root_folder + "/TestImages/Run%s") % num_folder_items + 1
 camera = PiCamera()
 camera.start_preview()
 sleep(5)
 
-for i in range(0,8):
+for i in range(0,9):
     camera.iso=(i*100)
-    camera.capture((root_folder + "/TestImages/Run%s/Original_Photo_%s.jpg") %(len(num_folders) + 1,i*100))
     sleep(5)
+    camera.capture((root_folder + "/TestImages/Run%s/Original_Photo_%s.jpg") %(num_folder_items + 1,i*100))
